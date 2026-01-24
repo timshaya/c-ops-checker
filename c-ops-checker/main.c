8c3e9d50ba5429044d1822d66060a0ebd14e2c4d
g t2 <17090087+timshaya@users.noreply.github.com>
2026-01-24 15:24:17 -0800

    initial commit

---
+++ c-ops-checker/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  c-ops-checker
+//
+//  Created by g t2 on 1/24/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    printf("Hello, c ops checker!\n");
+    return EXIT_SUCCESS;
+}
