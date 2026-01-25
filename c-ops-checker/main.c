5f3378c13d7db751f0f2dc761b46f4f4f077ebb0
g t2 <17090087+timshaya@users.noreply.github.com>
2026-01-24 17:17:26 -0800

    program control structure for io

--- c-ops-checker/main.c
+++ c-ops-checker/main.c
@@ -8,9 +8,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+char buffer[256];
+float _min =  0.0f;
+float _days = 0.0f;
+float _years = 0.0f;
+
+float convertMinToDays(float m) {
+   
+    float _d = (m * 60) * 24;
+    return _d;
+}
+
+float convertMinToYears(float m) {
     
-    printf("Hello, c ops checker!\n");
+    float _y = ((m * 60) * 24) * 356;
+    return _y;
+}
+
+void showView(float m) {
+    
+    printf("You entered this many minutes: %.2f\n\n",_min);
+    
+    _days = convertMinToDays(_min);
+    _years = convertMinToYears(_min);
+    
+    printf("Which is this many days: %.8f\n\nAnd this many years: %.8f\n\n", _days, _years);
+}
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Enter a number of minutes:\n");
+
+    while(fgets(buffer, sizeof buffer, stdin)) {
+        
+        //quit the app
+        if(buffer[0] == 'q') break;
+        
+        showView(_min);
+        printf("Enter minutes again or hit 'q' to quit:\n");
+    }
+    
     return EXIT_SUCCESS;
 }
